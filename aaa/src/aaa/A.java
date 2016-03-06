662b5aeae99ae151a05d6eef20ef346af4feaf78
Student <Student@10.0.11.142>
2016-03-06 14:30:45 +0200

    second commit

--- aaa/src/aaa/A.java
+++ aaa/src/aaa/A.java
@@ -4,7 +4,8 @@ public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("see the Bombers fly up!");
+		System.out.println("see the Bombers fly up up!");
+		System.out.println("to win the premiership flag");
 
 	}
 
